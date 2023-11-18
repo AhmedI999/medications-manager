8cec5d584f471dc9578c880ab51e121584921b90
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-11-18 22:04:34 +0200

    initial project code

---
+++ src/main/java/com/med/medicinesmanager/MedicinesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.med.medicinesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinesManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/med/medicinesmanager/MedicinesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.medicinesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinesManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
