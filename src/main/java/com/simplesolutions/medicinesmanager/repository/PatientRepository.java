f037e89dbd59c7b0c13c5f7de979c14f2196f1cd
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-11-20 22:28:25 +0200

    fixed warnings

--- src/main/java/com/simplesolutions/medicinesmanager/repository/PatientRepository.java
+++ src/main/java/com/simplesolutions/medicinesmanager/repository/PatientRepository.java
@@ -4,8 +4,6 @@ import com.simplesolutions.medicinesmanager.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 }
